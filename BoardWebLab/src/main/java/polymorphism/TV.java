45d5730c1ecd210bdf9de8234ec2aa64e43bc530
bigtyno <bigtyno2@gmail.com>
2021-07-01 22:55:12 +0900

    ..

---
+++ BoardWebLab/src/main/java/polymorphism/TV.java
@@ -0,0 +1,19 @@
+package polymorphism;
+
+public interface TV {
+	// interfacce 구성 멤버 
+	// 1. 상수
+	// 2. 추상 메소드
+	// 3. 디폴트 메소드
+	// 4. 정적 메소드
+    
+	
+	public void powerOn(); //추상메소드 메소드의 내용이 없기 때문에. (재정의해서 써야한다.)
+    
+    public void powerOff();
+    
+    public void volumeUp();
+    
+    public void volumeDown();
+
+}
